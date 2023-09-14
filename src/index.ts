d6df580d65614c165ec245bd7b261107b38864a5
Win Tongtawee <WinTongtawee@gmail.com>
2023-09-14 15:31:46 -0400

    bruh i forgot to cmd s

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,4 @@ const result_multiply = calc.multiply(2, 3);
 console.log(`result_multiply = ${result_multiply}`);
 const divide = calc.divide(2, 3);
 console.log(`divide = ${divide}`);
+calc.hello();
